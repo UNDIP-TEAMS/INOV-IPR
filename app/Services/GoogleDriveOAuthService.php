aa9c546c372424d25d999a1d4e00c9cc90c2526e
Aisha <aishasha1204@gmail.com>
2026-08-02 12:33:30 +0700

    benerin

--- app/Services/GoogleDriveOAuthService.php
+++ app/Services/GoogleDriveOAuthService.php
@@ -43,9 +43,9 @@ class GoogleDriveOAuthService
 
             $newToken = $client->fetchAccessTokenWithRefreshToken($refreshToken);
 
-            if (isset($newToken['error'])) {
-                throw new \Exception('Gagal me-refresh token Google. Silakan hubungkan ulang akun Google.');
-            }
+if (isset($newToken['error'])) {
+    throw new \Exception(json_encode($newToken));
+}
 
             $merged = array_merge($token, $newToken);
 
